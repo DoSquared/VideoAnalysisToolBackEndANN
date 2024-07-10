1677bf839ae81e7e45126b6698afa2bc5c939fb4
Mahdy <damahdy@gmail.com>
2024-07-10 11:01:42 +0200

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
-from django.urls import path
-from app.views import home, get_video_data, leg_raise_task, updatePlotData, update_landmarks
+from django.urls import path  # Import the path function from Django's urls module
+from app.views import home, get_video_data, leg_raise_task, updatePlotData, update_landmarks  # Import view functions from the app.views module
 
+# Define URL patterns and map them to view functions
 urlpatterns = [
-    path('', home),
-    path('video/', get_video_data),
-    path('leg_raise/', leg_raise_task),
-    path('update_plot/', updatePlotData),
-    path('update_landmarks/', update_landmarks),
-    path('toe_tap/',leg_raise_task)
+    path('', home),  # Root URL, maps to the home view
+    path('video/', get_video_data),  # URL for video data, maps to the get_video_data view
+    path('leg_raise/', leg_raise_task),  # URL for leg raise task, maps to the leg_raise_task view
+    path('update_plot/', updatePlotData),  # URL to update plot data, maps to the updatePlotData view
+    path('update_landmarks/', update_landmarks),  # URL to update landmarks, maps to the update_landmarks view
+    path('toe_tap/', leg_raise_task)  # URL for toe tap task, maps to the leg_raise_task view (reused)
 ]
