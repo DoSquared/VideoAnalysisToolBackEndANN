accc6f36e161b1a7c64fc6526ce1f910baeb7cd0
Mahdy <damahdy@gmail.com>
2024-07-10 16:46:40 +0200

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 
 def home(request):
+    """
+    Handle the home page view.
+
+    Parameters:
+    request (HttpRequest): The request object used to generate this response.
+
+    Returns:
+    HttpResponse: The response object containing the rendered template.
+    """
+    # Render the 'index.html' template and return the HTTP response
     return render(request, template_name='index.html')
