4775b9fa3947e859446bd8c179b4f33836b843a8
Mahdy <damahdy@gmail.com>
2024-07-10 16:48:47 +0200

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -8,9 +8,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the 'backend' project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+# Get the WSGI application callable
 application = get_wsgi_application()
